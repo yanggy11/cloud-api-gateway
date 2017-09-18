c63d30961c3f3b69244dee350929f60e32566654
yangguiyun <yangguiyun@mobanker.com>
2017-09-18 15:37:26 +0800

    add: cors filter

--- src/main/java/com/yanggy/cloud/ZuulApplication.java
+++ src/main/java/com/yanggy/cloud/ZuulApplication.java
@@ -1,10 +1,13 @@
 package com.yanggy.cloud;
 
+import com.yanggy.cloud.filter.CorsFilter;
 import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by yangguiyun on 2017/9/18.
@@ -14,6 +17,15 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableEurekaClient
 @SpringBootApplication
 public class ZuulApplication {
+    @Bean
+    public FilterRegistrationBean corsFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new CorsFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("corsFilter");
+        registration.setOrder(1);
+        return registration;
+    }
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
     }
