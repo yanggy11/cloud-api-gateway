cc3cf9dd4ef38bf7f9b24a615c090a2bc5ebff12
yangguiyun <yangguiyun@mobanker.com>
2017-10-17 15:15:47 +0800

    edit the spring security config and login return value

--- src/main/java/com/yanggy/cloud/auth/AuthController.java
+++ src/main/java/com/yanggy/cloud/auth/AuthController.java
@@ -2,11 +2,14 @@ package com.yanggy.cloud.auth;
 
 import com.yanggy.cloud.config.jwt.JWTUser;
 import com.yanggy.cloud.config.jwt.JwtTokenUtil;
+import com.yanggy.cloud.dto.ResponseEntity;
 import com.yanggy.cloud.mapper.UserMapper;
+import com.yanggy.cloud.param.UserParam;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -38,16 +42,19 @@ public class AuthController {
     @Value("${jwt.tokenHead}")
     private String tokenHead;
     @PostMapping(value="login")
-    public Object login(@RequestBody Map<String,String> map) {
-
-        UsernamePasswordAuthenticationToken upToken = new UsernamePasswordAuthenticationToken(map.get("username"), map.get("password"));
+    public ResponseEntity<?> login(@RequestBody UserParam userParam) {
+        UsernamePasswordAuthenticationToken upToken = new UsernamePasswordAuthenticationToken(userParam.getName(), userParam.getPassword());
         final Authentication authentication = authenticationManager.authenticate(upToken);
 
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(map.get("username"));
-        JWTUser jwtUser = (JWTUser) userDetails;
+        JWTUser jwtUser = (JWTUser) authentication.getPrincipal();
         final String token = this.tokenHead + jwtTokenUtil.generateToken(jwtUser);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        Map map = new HashMap();
+        map.put("token",token);
+        map.put("user", jwtUser);
 
-        return token;
+
+        return new ResponseEntity<>(map);
     }
 
 }
